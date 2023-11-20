99bc421b6121b46f0654ef2bc947f469f6bca1f5
white5588 <jonathanwisdom71@gmail.com>
2023-11-20 04:00:52 -0800

    main

--- freshFE/src/App.tsx
+++ freshFE/src/App.tsx
@@ -7,20 +7,17 @@ const App = () => {
 
   <div className='w-full h-full flex items-center flex-col'>
 
-<div className='w-full h-[80px] flex bg-[#3cb53c] justify-start items-start cursor-pointer flex-col'>
+<div className='w-full h-[80px] flex bg-[#3cb53ced] justify-start items-start cursor-pointer flex-col max-sm:hidden'>
 <div className='text-[white] text-[25px] font-semibold ml-5 mt-5'>Student</div>
 <div className='text-[white] text-[15px] ml-5'>Profile</div>
 
 </div>
 
-<div className='w-[70%] h-[450px] bg-white flex justify-center items-center mt-[4%] shadow-sm rounded border  flex-col'>
+<div className='w-[70%] h-[450px] bg-white flex justify-center items-center mt-[4%] shadow-sm rounded border  flex-col min-w-0'>
 
 <div className='w-full h-[200px] flex justify-between items-center '>
-  <div className='w-[17%] h-[170px] flex rounded-[50%] bg-[silver] ml-4 '>  
-  <img src={pics} className='w-full h-full rounded-full object-cover'/>
-  </div>
-  <div className='absolute ml-[17%] mt-[5%] font-bold '>Peter Oti
-  <div className='px-2 py-2 bg-[#41cc41] flex rounded-sm text-[10px] text-white cursor-pointer mt-5 '>Upload Image</div>
+  <div className='w-[17%] h-[170px] flex  text-[50px] font-semibold  rounded-tl-[15px] rounded-br-[15px] justify-center items-center text-[center] bg-[#4dba4d] text-white'>  PT</div>
+  <div className='absolute ml-[17%] mt-[5%] font-bold  '>Peter Oti
   </div>
   
   
@@ -30,7 +27,7 @@ const App = () => {
 
 </div>
 
-<div className='w-full h-[300px] flex bg-[silver] justify-between items-center '>
+<div className='w-full h-[300px] flex bg-[silver] justify-between items-center  '>
 
 
   <div className='w-[40%] h-[250px] flex bg-white justify-start items-start flex-col ml-5 rounded shadow-sm border leading-[35px]'>
@@ -58,6 +55,9 @@ const App = () => {
 
   <div className='mt-5 text-tl  text-[12px] font-semibold ml-2'>Date of Entry
   <div className='flex text-tl text-semibold text-[9px]'>11 November 2023</div>
+  </div>
+  <div className='mt-5 text-tl  text-[12px] font-semibold ml-2'>Bottles Achieved
+  <div className='flex text-tl text-semibold text-[9px]'>5,000</div>
   </div>
 <div className='mt-5 text-tl  text-[12px] font-semibold ml-2'>Status
 <div className='px-2 py-1 rounded bg-[#41cc41] mt-2 text-white cursor-pointer'>Active</div>
